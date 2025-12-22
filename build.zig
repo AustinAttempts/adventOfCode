669aeda0990cca4fa913087d86bb1fb71d14258b
Austin <austin.a.guevara@gmail.com>
2025-12-22 14:07:23 -0800

    inital build

--- build.zig
+++ build.zig
@@ -1,5 +1,33 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b; // stub
+    // Standard Configuration Options
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // create module
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // add vaxis dependency to module
+    const vaxis = b.dependency("vaxis", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe_mod.addImport("vaxis", vaxis.module("vaxis"));
+
+    //create executable
+    const exe = b.addExecutable(.{
+        .name = "adventOfCode",
+        .root_module = exe_mod,
+    });
+
+    b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_exe.step);
 }
